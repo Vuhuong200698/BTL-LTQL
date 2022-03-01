34cf3faefd227fa45ab903e29f04735cafd102c0
vuhuong200698 <vuhuong200698@gmail.com>
2022-03-02 00:11:03 +0700

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/SinhVienController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        // GET: SinhVien
+        public ActionResult index()
+        {
+            return View();
+
+        }
+        public ActionResult SinhVien()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SinhVien(string MaSinhVien, string HoTenSinhVien)
+        {
+            ViewBag.Thongtin = MaSinhVien + "" + HoTenSinhVien;
+            return View();
+        }
+    }
+}
